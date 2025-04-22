263dc198b17ef2cc44f47bfd6233562584818f12
Franko0101 <163135894+Franko0101@users.noreply.github.com>
2025-04-22 14:51:12 +0200

    Ability to generate a new grid.
    User can choose how big the new grid is.

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-const container = document.querySelector('#container');
+const GRID_SIZE = 960;
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'cell');
-    div.addEventListener('mouseover', () => {
-        div.setAttribute('style', 'background: red;');
-    });
+const container = document.querySelector('#container');
+const button = document.querySelector('#newGridButton');
+
+generateGrid(16);
+
+button.addEventListener('click', () => {
+    let newGridCellsPerSide = parseInt(prompt('Quante celle per lato vuoi che sia la tua griglia? (Massimo 100)'));
+    while (newGridCellsPerSide > 100) {
+        newGridCellsPerSide = parseInt(prompt('Hai inserito un valore maggiore di 100, riprova'));
+    }
+    deleteGrid();
+    generateGrid(newGridCellsPerSide);
+})
+
+//-----------------------------------------------------------//
+
+function generateGrid(cellsPerSide) {
+    const totalCells = cellsPerSide * cellsPerSide;
+    const cellSize = (GRID_SIZE / cellsPerSide) - 2;
+
+    for (let i = 0; i < totalCells; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('style', `
+            width: ${cellSize}px;
+            height: ${cellSize}px;
+            border: 1px solid gray;
+            `
+        );
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'red';
+        });
     
-    container.appendChild(div);
+        container.appendChild(div);
+    }
 }
+
+function deleteGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+
 
