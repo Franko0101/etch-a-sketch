870d1092d9b8e243ded81d98738264ce3765f6f3
Franko0101 <163135894+Franko0101@users.noreply.github.com>
2025-04-22 13:01:27 +0200

    Created grid with hover effect.
    Right now the grid is 16x16 fixed and the hover
    effect paints cells of red.

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('#container');
+
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'cell');
+    div.addEventListener('mouseover', () => {
+        div.setAttribute('style', 'background: red;');
+    });
+    
+    container.appendChild(div);
+}
+
